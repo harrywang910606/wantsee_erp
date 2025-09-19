a1808fd5270133f71f580fd31bd8b6cd177366ea
Harry.wang <Harrywang910606@gmail.com>
2025-09-20 03:03:25 +0800

    切換資料庫為 PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wantsee_erp',
+        'USER': 'wantsee',
+        'PASSWORD': '',  # 通常本地不設密碼
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
